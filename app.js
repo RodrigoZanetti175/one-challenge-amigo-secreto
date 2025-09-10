c6b3719ae10847131b7e087bed25ff8111abfcf2
Rodrigo <rodrigo.zanetti@hotmail.com>
2025-09-10 18:45:32 -0300

    feat: finished logic

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+function updateList(){
+    document.querySelector('ul#listaAmigos').innerHTML = ""
+    document.querySelector("ul#resultado").innerHTML = ""
+    amigos.forEach(element => {
+        const node = document.createElement("li");
+        const textnode = document.createTextNode(element);
+        node.appendChild(textnode)
+        document.querySelector('ul#listaAmigos').appendChild(node)
+    });
+}
+
+function adicionarAmigo(){
+    const amigo = document.querySelector('input#amigo').value
+    amigos.push(amigo)
+    updateList()
+}
+
+function sortearAmigo(){
+    const amigoSelecionado = amigos[Math.ceil(Math.random()*amigos.length)-1]
+    amigos = []
+    updateList()
+    document.querySelector("ul#resultado").innerHTML = amigoSelecionado
+}
